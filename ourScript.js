4c9cce5e2915d7232a231994047ee6bea5ad1e6c
BasmaM <92788812+FlouBsy@users.noreply.github.com>
2023-04-15 17:31:52 +0200

    Only General Functions

--- ourScript.js
+++ ourScript.js
@@ -77,7 +77,17 @@ function retrieveStudentFromLocalStorageByName(name) {
     });
     
     return Array.isArray(matchingStudents) ? matchingStudents : Array.from(matchingStudents);
-  }
+}
+
+// Function to retrieve students from local storage by name
+function retrieveStudentFromLocalStorageByID(id) {
+    var storedStudents = JSON.parse(localStorage.getItem("students")) || [];
+    var matchingStudents = storedStudents.filter(function(student) {
+      return student.ID === id;
+    });
+    
+    return Array.isArray(matchingStudents) ? matchingStudents : Array.from(matchingStudents);
+}
   
 // Function to edit a student in local storage by ID
 function editStudentInLocalStorage(id, newStudent) {
@@ -89,74 +99,4 @@ function editStudentInLocalStorage(id, newStudent) {
         storedStudents[index] = newStudent;
         localStorage.setItem("students", JSON.stringify(storedStudents));
     }
-}
-
-function searchAndShowStudent() {
-    var inputName = document.getElementById("SEARCH_NAME").value;
-    var students = retrieveStudentFromLocalStorageByName(inputName);
-    
-    if (students.length === 0) {
-        alert("There is no student with that name.");
-        return false;
-    }
-
-    var activeStudents = students.filter(function(student) {
-        return student.status === "active";
-    });
-
-    showStudent(activeStudents);
-}
-
-function clearTable(table)
-{
-    for(var i = 1;i<table.rows.length;){
-        table.deleteRow(i);
-    }
-}
-
-function showStudent(activeStudents)
-{
-    const table = document.getElementById("StudentTableinSearch");
-    clearTable(table);
-
-    activeStudents.forEach(function(student) {
-
-        const row = table.insertRow();
-        // Create a new cell for each property of the object you want to display
-        const nameCell = row.insertCell();
-        const IDCell = row.insertCell();
-        const lvlCell = row.insertCell();
-        const departCell = row.insertCell();
-      
-        // Insert the value of each property into the corresponding cell
-        IDCell.textContent = student.ID;
-        nameCell.textContent = student.name;
-        lvlCell.textContent = student.level;
-
-        if (student.level >= 3) {
-            const anchor = document.createElement("a");
-            if (student.department == ""){
-                anchor.textContent = "Click Here to Assign Your Department";
-            }
-            else
-            {
-                anchor.textContent = student.department;
-            }
-            anchor.href = "DepartmentAssignment.htm";
-            anchor.target = "_blank"; // Opens the link in a new tab or window
-            anchor.onclick = function() {
-              window.open("DepartmentAssignment.htm", "myWindow", "width=500,height=500");
-              return false;
-            };
-            departCell.appendChild(anchor);
-          } else {
-            departCell.textContent = student.department;
-          }
-          
-    });
-}
-
-function assignDepartment()
-{
-    
 }
